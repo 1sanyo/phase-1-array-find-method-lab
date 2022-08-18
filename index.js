db5d04658e854c7bd3a8e5ebe46a1da925aca054
1sanyo <sanyo.adonai@student.moringaschool.com>
2022-08-18 13:49:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const results = record.find(x => x.result === "W");
+    console.log(results);
+    if (results) {
+        return results.year;
+    } else {
+        return undefined;
+    }
+}
